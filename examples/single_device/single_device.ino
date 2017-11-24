274383a6a9c905102b18fcc534ad77026347b385
J.R. Leeman <kd5wxb@gmail.com>
2017-11-24 15:29:42 -0700

    Improve single device example

--- examples/single_device/single_device.ino
+++ examples/single_device/single_device.ino
@@ -1,4 +1,5 @@
 #include "AD5293.h"
+#include "SPI.h"
 
 const uint8_t CS_PIN = 6;
 AD5293 digPot;
@@ -7,21 +8,14 @@ void setup() {
   Serial.begin(115200);
   digPot.begin(1, CS_PIN);
   Serial.println("Setup complete!");
+  Serial.println("Enabling write.");
+  digPot.configure(0, 0, 1); // First device in performance mode and write enabled
 }
 
 void loop() {
-  Serial.println("Enabling write.");
-  digPot.configure(0, 0, 1); // First device in performance mode and write enabled
-  Serial.println("Setting to position 0");
-  digPot.setWiperPosition(0, 0);
-  delay(5000);
-  Serial.println("Setting to position 256");
-  digPot.setWiperPosition(0, 256);
-  delay(5000);
-  Serial.println("Setting to position 512");
-  digPot.setWiperPosition(0, 512);
-  delay(5000);
-  Serial.println("Setting to position 1023");
-  digPot.setWiperPosition(0, 1023);
-  delay(5000);
+  for (int i=0; i<1024; i++){
+    Serial.println(i);
+    digPot.setWiperPosition(0, i);
+    delay(100);
+  }
 }
